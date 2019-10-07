473ac335ea2028ebf495460fcfb2f2927463e7e1
Mridul Gupta <mridul.gupta@gmail.com>
2019-10-07 12:54:07 +1100

    learning flexbox

--- App.js
+++ App.js
@@ -14,38 +14,43 @@ export default function App() {
 
       </View>
 
-    <View style={{padding: 50}}>
-      <View 
-        style={{
-          backgroundColor: 'red',
-          width: 100,
-          height: 100,
-          justifyContent: 'center',
-          alignItems: 'center'
-        }}>
+      <View style={
+        {
+          padding: 50,
+          flexDirection: "row",
+          width: '80%',
+          height: 300,
+          justifyContent: 'space-around',
+          alignItems: 'stretch'
+        }
+      }>
+        <View
+          style={{
+            backgroundColor: 'red',
+            flex: 3,
+            justifyContent: 'center',
+            alignItems: 'center'
+          }}>
           <Text>1</Text>
-      </View>
-      <View 
-        style={{
-          backgroundColor: 'blue',
-          width: 100,
-          height: 100,
-          justifyContent: 'center',
-          alignItems: 'center'
-        }}>
+        </View>
+        <View
+          style={{
+            backgroundColor: 'blue',
+            flex: 2,
+            justifyContent: 'center',
+            alignItems: 'center'
+          }}>
           <Text>2</Text>
-      </View>
-      <View 
-        style={{
-          backgroundColor: 'green',
-          width: 100,
-          height: 100,
-          justifyContent: 'center',
-          alignItems: 'center'
-        }}>
+        </View>
+        <View
+          style={{
+            backgroundColor: 'green',
+            justifyContent: 'center',
+            alignItems: 'center'
+          }}>
           <Text>3</Text>
+        </View>
       </View>
-    </View>
 
     </View>
   );
